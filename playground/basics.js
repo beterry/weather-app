44c517e0057af4491dd04d3ddf3d55f21ee592b4
Ben Terry <benterry@Bens-MacBook.local>
2019-01-25 14:27:04 -0500

    Initial commit

---
+++ playground/basics.js
@@ -0,0 +1,12 @@
+console.log('Starting app...');
+
+setTimeout(() => {
+  console.log('Inside callback');
+}, 2000);
+
+
+setTimeout(() => {
+  console.log('Second callback');
+}, 0);
+
+console.log('Finishing up...');
