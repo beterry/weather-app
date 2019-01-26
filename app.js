6a4009002b19d9f7035ebcdc37b0693ae5689556
Ben Terry <beterry12@gmail.com>
2019-01-26 11:38:01 -0500

    Added user input

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather for',
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+
+let address = encodeURIComponent(argv.a); 
 
 request({
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadephia&key=AIzaSyD2KT__x0Gy8QHNbZhJfZfgq2q2RTGcseE',
+  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=AIzaSyD2KT__x0Gy8QHNbZhJfZfgq2q2RTGcseE`,
   json: true
 }, (error, response, body) => {
-  console.log(body);
+  console.log(`Address: ${body.results[0].formatted_address}`);
+  console.log(`LAT: ${body.results[0].geometry.location.lat}`);
+  console.log(`LNG: ${body.results[0].geometry.location.lng}`);
 })
